b5ea16531aff1005048c9815e83c83fb05784e76
abaez8031 <abaez8031@gmail.com>
2023-04-16 16:17:18 -0400

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
 
 const cors = require('cors');
 const csurf = require('csurf');
@@ -34,5 +35,24 @@ app.use(
 app.use('/api/users', usersRouter);
 app.use('/api/games', gamesRouter);
 app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
