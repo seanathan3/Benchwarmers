3035458e3e0dfb0a9aa95db8e9feb7e45ae9dee5
abaez8031 <abaez8031@gmail.com>
2023-04-16 15:33:22 -0400

    gamesrouter

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/api/users');
+const gamesRouter = require('./routes/api/games');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/games', gamesRouter);
 
 module.exports = app;
