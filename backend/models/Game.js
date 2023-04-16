359bb0b961c7afcd57adbf0a39afe7fc1f0a832e
abaez8031 <abaez8031@gmail.com>
2023-04-16 17:06:22 -0400

    schemas

---
+++ backend/models/Game.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema ({
+  coordinates: {
+    lat: Number,
+    lng: Number,
+    required: true
+  },
+  sport: {
+    type: String,
+    required: true
+  },
+  skillLevel: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  host: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  attendees: [
+    {type: Schema.Types.ObjectId, ref: "User"}
+  ],
+  maxCapacity: {
+    type: Number,
+    required: true
+  },
+  minCapacity: {
+    type: Number,
+    required: true
+  },
+  photoUrl: {
+    type: String
+  },
+  time: {
+    hours: Number,
+    minutes: Number,
+    required: true
+  },
+  date: {
+    day: Number,
+    month: Number,
+    year: Number,
+    required: true
+  }
+
+})
