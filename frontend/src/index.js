8cc29c5d977fa77e2ae67001aec1b64af8afcec9
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-04-16 18:26:38 -0400

    frontend setup

---
+++ frontend/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store/store';
+
+let store = configureStore({});
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
