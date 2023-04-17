f97ec4ac264dd3c193f35a84675e6193a307704f
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-04-17 15:37:10 -0400

    user profile updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,10 @@ import Splash from "./components/Splash/Splash";
 import NavBar from "./components/NavBar/NavBar";
 import LoginForm from "./components/SessionForms/LoginForm";
 import SignupForm from "./components/SessionForms/SignupForm";
-<<<<<<< HEAD
 import UserProfile from "./components/UserProfile/UserProfile";
-=======
 import { getCurrentUser } from "./store/session";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
->>>>>>> 443a5489db4a62df08f1c19b9a82be5fef331ce6
 
 function App() {
   const dispatch = useDispatch();
@@ -21,7 +18,6 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
     <NavBar />
     <LoginForm />
     <SignupForm />
@@ -31,14 +27,6 @@ function App() {
     <Route path="/user-profile">
       <UserProfile />
     </Route>
-=======
-      <NavBar />
-      <LoginForm />
-      <SignupForm />
-      <Route path="/">
-        <Splash />
-      </Route>
->>>>>>> 443a5489db4a62df08f1c19b9a82be5fef331ce6
     </>
   );
 }
--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -9,20 +9,14 @@ const UserProfile = ({ userData }) => {
   const [address, setAddress] = useState("");
   const [profilePic, setProfilePic] = useState();
   const [faveSport, setFaveSport] = useState();
-  const [selectedOption, setSelectedOption] = useState();
   const [checkedItems, setCheckedItems] = useState([]);
 
-  const handleCheckboxChange = (e) => {
-    const value = e.target.value;
-    if (e.target.checked) {
-      setCheckedItems([...checkedItems, value]);
-    } else {
-      setCheckedItems(checkedItems.filter((item) => item !== value));
-    }
+  const handleBoroughChange = (e) => {
+    setBorough(e.target.value);
   };
 
-  const handleOptionChange = (e) => {
-    setSelectedOption(e.target.value);
+  const handleFaveSportChange = (e) => {
+    setFaveSport(e.target.value);
   };
 
   // const handleSubmit = (e) => {
@@ -46,10 +40,12 @@ const UserProfile = ({ userData }) => {
     <>
       <h2>Account Information</h2>
       <div id="up-information">
-        <div id="up-name">
+        <div id="up-name">Name: 
+        <br></br>
           <input type="text" id="up-name"></input>
         </div>
-        <div id="up-email">
+        <div id="up-email">Email: 
+        <br></br>
           <input
             type="text"
             name="email"
@@ -57,7 +53,8 @@ const UserProfile = ({ userData }) => {
             onChange={(e) => setEmail(e.target.value)}
           ></input>
         </div>
-        <div id="up-username">
+        <div id="up-username">Username: 
+        <br></br>
           <input
             type="text"
             name="username"
@@ -65,26 +62,35 @@ const UserProfile = ({ userData }) => {
             onChange={(e) => setUsername(e.target.value)}
           ></input>
         </div>
-        <div id="up-bio">
+        <div id="up-bio">Bio:
+        <br></br>
           <textarea
             name="bio"
             value={bio}
             onChange={(e) => setBio(e.target.value)}
           ></textarea>
-        </div> Address
-        <div id="up-address">
-          <input id="up-street" placeholder="street number"></input>
+        </div> 
+        <br />
+
+        <div id="up-address">Address: 
+        <br></br>
+          <input id="up-street" placeholder="street information"></input>
+          <br />
           <input id="up-city" placeholder="city"></input>
+          <br />
           <input id="up-state" placeholder="State"></input>
+          <br />
           <input id="up-zip" placeholder="zipcode"></input>
         </div>
+        <br />
         <div id="up-borough">
-          <label htmlFor="dropdown">Select your borough</label>
+          <label htmlFor="dropdown">Select your borough: </label>
           <select
             id="dropdown"
-            value={selectedOption}
-            onChange={handleOptionChange}
+            value={borough}
+            onChange={handleBoroughChange}
           >
+            <option value="" disabled selected>Select</option>
             <option value="bronx">Bronx</option>
             <option value="brooklyn">Brooklyn</option>
             <option value="manhattan">Manhattan</option>
@@ -93,216 +99,34 @@ const UserProfile = ({ userData }) => {
           </select>
         </div>
         <div id="up-fave-sport">
-          <label htmlFor="dropdown">Favorite Sport</label>
+          <label htmlFor="sport-dropdown">Favorite Sport: </label>
           <select
             id="sport-dropdown"
-            value={selectedOption}
-            onChange={setFaveSport}
+            value={faveSport}
+            onChange={handleFaveSportChange}
           >
+            <option value="" disabled selected>Select</option>
             <option value="badminton">Badminton</option>
             <option value="baseball">Baseball</option>
-            <option value="manhattan">Manhattan</option>
-            <option value="queens">Queens</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
-            <option value="staten-island">Staten Island</option>
+            <option value="basketball">Basketball</option>
+            <option value="cyling">Cycling</option>
+            <option value="darts">Darts</option>
+            <option value="fencing">Fencing</option>
+            <option value="football">Football</option>
+            <option value="golf">Golf</option>
+            <option value="hand-ball">Hand Ball</option>
+            <option value="hockey">Hockey</option>
+            <option value="martial-arts">Martial Arts</option>
+            <option value="soccer">Soccer</option>
+            <option value="softball">Softball</option>
+            <option value="squash">Squash</option>
+            <option value="table-tennis">Table Tennis</option>
+            <option value="tennis">Tennis</option>
+            <option value="volleyball">Volleyball</option>
+            <option value="other">Other</option>
           </select>
         </div>
-        <div>
-          <label>Choose your favorite sport:</label>
-          <div>
-            <input
-              type="checkbox"
-              id="football"
-              value="football"
-              checked={checkedItems.includes("football")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="football">Footbll</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="baseball"
-              value="baseball"
-              checked={checkedItems.includes("baseball")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="baseball">Baseball</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="basketball"
-              value="basketball"
-              checked={checkedItems.includes("basketball")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="basketball">Basketball</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="tennis"
-              value="tennis"
-              checked={checkedItems.includes("tennis")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="tennis">Tennis</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="soccer"
-              value="soccer"
-              checked={checkedItems.includes("soccer")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="soccer">Soccer</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="softball"
-              value="softball"
-              checked={checkedItems.includes("softball")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="softball">Softball</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="badminton"
-              value="badmi"
-              checked={checkedItems.includes("badminton")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="badminton">Badminton</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="volleyball"
-              value="volleyball"
-              checked={checkedItems.includes("volleyball")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="volleyball">Volleyball</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="cycling"
-              value="cycling"
-              checked={checkedItems.includes("cycling")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="cycling">Cycling</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="darts"
-              value="darts"
-              checked={checkedItems.includes("darts")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="darts">Darts</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="fencing"
-              value="fencing"
-              checked={checkedItems.includes("fencing")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="fencing">Fencing</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="golf"
-              value="golf"
-              checked={checkedItems.includes("golf")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="golf">Golf</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="hand-ball"
-              value="hand-ball"
-              checked={checkedItems.includes("hand-ball")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="hand-ball">Hand Ball</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="hockey"
-              value="hockey"
-              checked={checkedItems.includes("hockey")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="hockey">Hockey</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="martial-arts"
-              value="martial-arts"
-              checked={checkedItems.includes("martial-arts")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="martial-arts">Martial Arts</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="squash"
-              value="squash"
-              checked={checkedItems.includes("squash")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="squash">Squash</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="table-tennis"
-              value="table-tennis"
-              checked={checkedItems.includes("table-tennis")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="table-tennis">Table Tennis</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="other"
-              value="other"
-              checked={checkedItems.includes("other")}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="other">Other</label>
-          </div>
           <div>Profile Picture Upload</div>
-        </div>
       </div>
     </>
   );
