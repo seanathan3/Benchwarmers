d34643107624aa5159b4e15315106fc81482a345
Charles Cruse <charle9@vt.edu>
2023-04-18 17:21:26 -0400

    cleanup Map.js, app.js, index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import { Switch, Route } from "react-router-dom";
 import Splash from "./components/Splash/Splash";
 import NavBar from "./components/NavBar/NavBar";
-import LoginForm from "./components/SessionForms/LoginForm";
-import SignupForm from "./components/SessionForms/SignupForm";
 import UserShowPage from "./components/UpdateUserProfile/UserShowPage";
 import { getCurrentUser } from "./store/session";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import GamesIndex from "./components/GamesIndex/GamesIndex";
-import Map from "./components/Map/Map";
 
 function App() {
   const dispatch = useDispatch();
