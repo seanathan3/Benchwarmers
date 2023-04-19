7a5d664b25c675c43111931c1e7d07c3629df20a
Sean Abbas <sean.abbas33@gmail.com>
2023-04-19 10:19:47 -0400

    games show start

--- frontend/src/components/GamesIndex/IndexItem.jsx
+++ frontend/src/components/GamesIndex/IndexItem.jsx
@@ -1,23 +1,27 @@
-import badmintonImg from '../../assets/sports-logos/badminton.png'
-import baseballImg from '../../assets/sports-logos/baseball.png'
-import basketballImg from '../../assets/sports-logos/basketball.png'
-import cyclingImg from '../../assets/sports-logos/cycling.png'
-import dartsImg from '../../assets/sports-logos/darts.png'
-import fencingImg from '../../assets/sports-logos/fencing.png'
-import footballImg from '../../assets/sports-logos/football.png'
-import golfImg from '../../assets/sports-logos/golf.png'
-import handBallImg from '../../assets/sports-logos/hand_ball.png'
-import hockeyImg from '../../assets/sports-logos/hockey.png'
-import martialArtsImg from '../../assets/sports-logos/martial_arts.png'
-import otherImg from '../../assets/sports-logos/other.png'
-import soccerImg from '../../assets/sports-logos/soccer.png'
-import softballImg from '../../assets/sports-logos/softball.png'
-import tableTennisImg from '../../assets/sports-logos/table_tennis.png'
-import tennisImg from '../../assets/sports-logos/tennis.png'
-import volleyballImg from '../../assets/sports-logos/volleyball.png'
+import { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import GamesShow from '../GamesShow/GamesShow';
+import badmintonImg from '../../assets/sports-logos/badminton.png';
+import baseballImg from '../../assets/sports-logos/baseball.png';
+import basketballImg from '../../assets/sports-logos/basketball.png';
+import cyclingImg from '../../assets/sports-logos/cycling.png';
+import dartsImg from '../../assets/sports-logos/darts.png';
+import fencingImg from '../../assets/sports-logos/fencing.png';
+import footballImg from '../../assets/sports-logos/football.png';
+import golfImg from '../../assets/sports-logos/golf.png';
+import handBallImg from '../../assets/sports-logos/hand_ball.png';
+import hockeyImg from '../../assets/sports-logos/hockey.png';
+import martialArtsImg from '../../assets/sports-logos/martial_arts.png';
+import otherImg from '../../assets/sports-logos/other.png';
+import soccerImg from '../../assets/sports-logos/soccer.png';
+import softballImg from '../../assets/sports-logos/softball.png';
+import tableTennisImg from '../../assets/sports-logos/table_tennis.png';
+import tennisImg from '../../assets/sports-logos/tennis.png';
+import volleyballImg from '../../assets/sports-logos/volleyball.png';
 
 
 const IndexItem = ({game}) => {
+    const [showModal, setShowModal] = useState(false);
 
     let image;
 
@@ -77,14 +81,22 @@ const IndexItem = ({game}) => {
 
 
     return(
-        <div id="ii-master">
-            <img id="ii-image" src={image} alt="sports ball" />
-            <div id="ii-desc">
-                <p id="ii-sport">{game.sport[0].toUpperCase() + game.sport.slice(1)}</p>
-                <p>Host: {game.host?.username}</p>
-                <p>{game.date.month}/{game.date.day}/{game.date.year} @ {game.time.hours}:{game.time.minutes}</p>
+        <>
+            <div id="ii-master" onClick={() => setShowModal(true)}>
+                <img id="ii-image" src={image} alt="sports ball" />
+                <div id="ii-desc">
+                    <p id="ii-sport">{game.sport[0].toUpperCase() + game.sport.slice(1)}</p>
+                    <p>Host: {game.host?.username}</p>
+                    <p>{game.date.month}/{game.date.day}/{game.date.year} @ {game.time.hours}:{game.time.minutes}</p>
+                </div>
             </div>
-        </div>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <GamesShow game={game} />
+                </Modal>
+            )}
+        </>
+
     )
 };
 
