83797d7edc1438d59d63447f3f2cbf887b01bff2
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-04-25 17:37:41 -0400

    user profile games component styling

--- frontend/src/components/UpcomingGames/UpcomingGames.jsx
+++ frontend/src/components/UpcomingGames/UpcomingGames.jsx
@@ -26,9 +26,14 @@ const upcomingGames = useSelector(getUpcomingGames);
   }, [dispatch])
 
   return (
-    <div id='scroll-wrapper'>
+    
+    <>
+
+    <div id='scroll-wrapper'>Upcoming Games:
+    <br />
+    <br />
+      {/* <label id='ug-title'>Upcoming Games:</label> */}
     <div id='upcoming'>
-      <label id='ug-title'>Upcoming Games:</label>
       <div id='ug-card-container'>
         {
           upcomingGames.map((game) => {
@@ -39,6 +44,7 @@ const upcomingGames = useSelector(getUpcomingGames);
       </div>
       </div>
     </div>
+    </>
   )
 
 };
